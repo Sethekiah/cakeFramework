3df728351df15205e7c009152446ddd7019b91b5
whitmerc <whityj375@gmail.com>
2025-09-22 15:20:48 -0400

    feat: added introduction to mainpage

--- docs/MAIN.md
+++ docs/MAIN.md
@@ -1,5 +1,26 @@
-# cakeFramework Documentation {#mainpage}
+## cakeFramework Documentation {#mainpage}
 
-### Introduction
+#### Introduction
 
-cakeFramework is...
+---
+
+CakeFramework is an extension / layer on top of the SFML library for C++. It provides SFML developers with an entity-component-system (ECS) architecture with a collection of common components to supply the tools required for more advanced game development.
+<br><br>
+Please see the [repository](https://github.com/Sethekiah/cakeFramework) for installation instructions.
+
+#### Example
+
+---
+
+Starter Code:
+
+```
+import cakeFramework;
+import std;
+
+int main() {
+	// Insert actual code here
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
+```
--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['enable_0',['enable',['../classComponent.html#a4427c5c25ebb086bf68e950fdd3c5424',1,'Component::enable()'],['../classEntity.html#ab8af282d7a381bb7a4adcea3f41732e0',1,'Entity::enable()']]],
   ['end_1',['end',['../classGame.html#a0d6ebf09030504b5139df08767393aa6',1,'Game']]],
-  ['entity_2',['entity',['../classEntity.html',1,'Entity'],['../classEntity.html#a9665c9a50cbdf79365ead67052a11c63',1,'Entity::Entity(void)'],['../classEntity.html#a872611a05e522d043c8995f34dbbb48b',1,'Entity::Entity(Entity &amp;parent)'],['../group__Entity.html',1,'Entity']]]
+  ['entity_2',['entity',['../classEntity.html',1,'Entity'],['../classEntity.html#a9665c9a50cbdf79365ead67052a11c63',1,'Entity::Entity(void)'],['../classEntity.html#a872611a05e522d043c8995f34dbbb48b',1,'Entity::Entity(Entity &amp;parent)'],['../group__Entity.html',1,'Entity']]],
+  ['example_3',['Example',['../index.html#autotoc_md2',1,'']]]
 ];
